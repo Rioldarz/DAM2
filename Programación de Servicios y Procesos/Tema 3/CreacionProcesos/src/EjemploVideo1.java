70901f790f3bcaffba96a0330b37266addd01af0
Miguel Ángel Rodríguez Dalí <migueadali@gmail.com>
2022-10-20 19:58:10 +0200

    Ejemplo del vídeo 1 terminado

--- Programación de Servicios y Procesos/Tema 3/CreacionProcesos/src/EjemploVideo1.java
+++ Programación de Servicios y Procesos/Tema 3/CreacionProcesos/src/EjemploVideo1.java
@@ -1,7 +1,9 @@
+import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class EjemploVideo1 {
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		Scanner teclado = new Scanner(System.in);
 		int numero1, numero2;
@@ -15,12 +17,28 @@ public class EjemploVideo1 {
 			int estado_ejecucion = ejecutarClaseProceso(Sumador.class, numero1, numero2);
 			if (estado_ejecucion == 0) {
 				System.out.println("Proceso ejecutado correctamente.");
-			}else {
+			} else {
 				System.out.println("Error ejecutando el proceso");
-			}//Fin Si
-		}catch (IOException | InterruptedException ex) {
+			} // Fin Si
+		} catch (IOException | InterruptedException ex) {
 			System.err.println("Error: " + ex.toString());
 			System.exit(-1);
-		}//Fin try
-	}
+		} // Fin try
+	}// Fin Programa
+
+	@SuppressWarnings("rawtypes")
+	public static int ejecutarClaseProceso(Class clase, int n1, int n2) throws IOException, InterruptedException {
+		String javaHome = System.getProperty("java.home");
+		String javaBin = javaHome + File.separatorChar + "bin" + File.separatorChar + "java";
+
+		String classPath = System.getProperty("java.class.path");
+		String className = clase.getCanonicalName();
+
+		ProcessBuilder builder = new ProcessBuilder(javaBin, "-cp", classPath, className, String.valueOf(n1),
+				String.valueOf(n2));
+		Process process = builder.start();
+		
+		process.waitFor();
+		return process.exitValue();
+	}//Fin Función
 }
--- Programación de Servicios y Procesos/Tema 3/CreacionProcesos/src/Sumador.java
+++ Programación de Servicios y Procesos/Tema 3/CreacionProcesos/src/Sumador.java
@@ -1,12 +1,5 @@
 
-public class Sumar {
-	/**
-	 * Este método suma todos los números que hay en un intervalo
-	 * 
-	 * @param numero1 Inicio del intervalo
-	 * @param numero2 Fin del intervalo
-	 * @return Suma de todos los números que hay en el intervalo definido
-	 */
+public class Sumador {
 	public static int sumar(int numero1, int numero2) {
 		int suma = 0;
 		for (int i = numero1; i <= numero2; i++) {
@@ -15,12 +8,11 @@ public class Sumar {
 		return suma;
 	}// Fin Función
 
+	@SuppressWarnings("unused")
 	public static void main(String[] args) {
-		// Obtenemos los parámetros pasados al crear el proceso
 		int numero1 = Integer.valueOf(args[0]);
 		int numero2 = Integer.valueOf(args[1]);
 
 		int suma = sumar(numero1, numero2);
 	}// Fin Programa
-
 }
