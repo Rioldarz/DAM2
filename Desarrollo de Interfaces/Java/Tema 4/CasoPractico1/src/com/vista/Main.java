bf1c58a37081d385e1b77798299e649c299dd65d
Miguel Ángel Rodríguez Dalí <migueadali@gmail.com>
2022-10-26 19:29:36 +0200

    CasoPractico1 DDI tema 4 hecho junto al jar

--- Desarrollo de Interfaces/Java/Tema 4/CasoPractico1/src/com/vista/Main.java
+++ Desarrollo de Interfaces/Java/Tema 4/CasoPractico1/src/com/vista/Main.java
@@ -5,7 +5,6 @@ import java.awt.EventQueue;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -17,14 +16,17 @@ import javax.swing.border.EmptyBorder;
 
 import com.modelo.GestionaRecargas;
 import com.toedter.calendar.JMonthChooser;
+import java.awt.Font;
 
+public class Main extends JFrame {
 
-
-public class Usuario extends JFrame {
-
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTextField dineroIntroducido;
-	
+
 	/**
 	 * Launch the application.
 	 */
@@ -32,7 +34,7 @@ public class Usuario extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Usuario frame = new Usuario();
+					Main frame = new Main();
 					Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 					frame.setLocation(dim.width / 2 - frame.getSize().width / 2,
 							dim.height / 2 - frame.getSize().height / 2);
@@ -47,7 +49,7 @@ public class Usuario extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public Usuario() {
+	public Main() {
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 350, 300);
@@ -58,43 +60,55 @@ public class Usuario extends JFrame {
 		contentPane.setLayout(null);
 
 		JLabel fotoMetro = new JLabel("");
-		fotoMetro.setIcon(new ImageIcon(Usuario.class.getResource("/com/resources/Metro_petit.jpg")));
+		fotoMetro.setIcon(new ImageIcon(Main.class.getResource("/com/resources/Metro_petit.jpg")));
 		fotoMetro.setBounds(50, 50, 250, 125);
 		contentPane.add(fotoMetro);
 
 		JPanel panel = new JPanel();
-		panel.setBounds(190, 178, 110, 30);
+		panel.setBounds(220, 178, 110, 30);
 		contentPane.add(panel);
 
 		JMonthChooser monthChooser = new JMonthChooser();
 		panel.add(monthChooser);
 
 		JLabel dinero = new JLabel("0€");
-		dinero.setBounds(184, 214, 45, 13);
+		dinero.setBounds(130, 212, 80, 13);
 		contentPane.add(dinero);
 
 		dineroIntroducido = new JTextField();
-		dineroIntroducido.setBounds(50, 210, 96, 19);
+		dineroIntroducido.setBounds(20, 208, 96, 19);
 		contentPane.add(dineroIntroducido);
 		dineroIntroducido.setColumns(10);
 
+		GestionaRecargas gr = new GestionaRecargas();
+
 		JLabel texto = new JLabel("Introduzca cantidad");
-		texto.setBounds(50, 186, 130, 13);
+		texto.setBounds(10, 186, 130, 13);
 		contentPane.add(texto);
 		JButton aceptar = new JButton("Aceptar");
 		aceptar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				GestionaRecargas gr = new GestionaRecargas();
-				
 				int temp1 = monthChooser.getMonth();
 				String temp2 = dineroIntroducido.getText();
 				gr.setMes(temp1, temp2);
 				int salida = gr.getMes(temp1);
+
 				dinero.setText(salida + "€");
-				
 			}
 		});
 		aceptar.setBounds(130, 235, 85, 21);
 		contentPane.add(aceptar);
+
+		JButton selecMes = new JButton("Seleccionar mes");
+		selecMes.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		selecMes.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int temp1 = monthChooser.getMonth();
+				int salida = gr.getMes(temp1);
+				dinero.setText(salida + "€");
+			}
+		});
+		selecMes.setBounds(220, 209, 110, 23);
+		contentPane.add(selecMes);
 	}
 }
