863031fe55a39f8c8b975b13105b309084ca65a2
Miguel Ángel Rodríguez Dalí <migueadali@gmail.com>
2022-10-26 11:42:15 +0200

    pkmn y practica interfaz metro avanzada

--- Desarrollo de Interfaces/Java/Tema 4/CasoPractico1/src/com/vista/Interfaz.java
+++ Desarrollo de Interfaces/Java/Tema 4/CasoPractico1/src/com/vista/Interfaz.java
@@ -7,11 +7,16 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
-import de.wannawork.jcalendar.JCalendarPanel;
+import com.toedter.calendar.JMonthChooser;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
 
 public class Interfaz extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField dineroIntroducido;
 
 	/**
 	 * Launch the application.
@@ -41,17 +46,38 @@ public class Interfaz extends JFrame {
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLabel fotoMetro = new JLabel("");
 		fotoMetro.setIcon(new ImageIcon(Interfaz.class.getResource("/com/resources/Metro_petit.jpg")));
 		fotoMetro.setBounds(50, 50, 250, 125);
 		contentPane.add(fotoMetro);
-		
+
 		JPanel panel = new JPanel();
-		panel.setBounds(442, 50, 316, 454);
+		panel.setBounds(514, 220, 126, 29);
 		contentPane.add(panel);
-		
-		JCalendarPanel Calendario = new JCalendarPanel();
-		panel.add(Calendario);
+
+		JMonthChooser monthChooser = new JMonthChooser();
+		panel.add(monthChooser);
+
+		JLabel dinero = new JLabel("0€");
+		dinero.setBounds(607, 292, 45, 13);
+		contentPane.add(dinero);
+
+		dineroIntroducido = new JTextField();
+		dineroIntroducido.setBounds(433, 289, 96, 19);
+		contentPane.add(dineroIntroducido);
+		dineroIntroducido.setColumns(10);
+
+		JLabel texto = new JLabel("Introduzca cantidad");
+		texto.setBounds(433, 266, 96, 13);
+		contentPane.add(texto);
+		JButton aceptar = new JButton("Aceptar");
+		aceptar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dinero.setText(dineroIntroducido.getText());
+			}
+		});
+		aceptar.setBounds(492, 365, 85, 21);
+		contentPane.add(aceptar);
 	}
 }
