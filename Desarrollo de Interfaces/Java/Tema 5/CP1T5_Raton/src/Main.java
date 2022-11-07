4e4eb1cd5f1e4a034bcdb30a475abcc7c1c5a535
Miguel Ángel Rodríguez Dalí <migueadali@gmail.com>
2022-11-07 18:18:11 +0100

    Interfaz base para modificador ratón

--- Desarrollo de Interfaces/Java/Tema 5/CP1T5_Raton/src/Main.java
+++ Desarrollo de Interfaces/Java/Tema 5/CP1T5_Raton/src/Main.java
@@ -1,12 +1,23 @@
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+import javax.swing.SwingConstants;
 
 public class Main extends JFrame {
 
-	private JPanel contentPane;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private int xMouse, yMouse;
+	private JFrame frame;
 
 	/**
 	 * Launch the application.
@@ -15,8 +26,9 @@ public class Main extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Main frame = new Main();
-					frame.setVisible(true);
+					Main window = new Main();
+					window.frame.setLocationRelativeTo(null);
+					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -28,12 +40,59 @@ public class Main extends JFrame {
 	 * Create the frame.
 	 */
 	public Main() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		frame = new JFrame();
+		frame.setUndecorated(true);
+		frame.setResizable(false);
+		frame.setBounds(100, 100, 800, 600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
 
-		setContentPane(contentPane);
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.LIGHT_GRAY);
+		panel.addMouseMotionListener(new MouseMotionAdapter() {
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				int x = e.getXOnScreen();
+				int y = e.getYOnScreen();
+				frame.setLocation(x - xMouse, y - yMouse);
+			}
+		});
+		panel.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				xMouse = e.getX();
+				yMouse = e.getY();
+			}
+		});
+		panel.setBounds(0, 0, 800, 30);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(Color.LIGHT_GRAY);
+		panel_1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				System.exit(0);
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				panel_1.setBackground(Color.red);
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				panel_1.setBackground(Color.LIGHT_GRAY);
+			}
+		});
+		panel_1.setBounds(750, 0, 50, 30);
+		panel.add(panel_1);
+
+		JLabel x = new JLabel("X");
+		x.setFont(new Font("Dialog", Font.BOLD, 15));
+		x.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_1.add(x);
 	}
 
 }
