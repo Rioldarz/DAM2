c06267809d7d5cee061cbf9057ad6431bcfed7a3
Miguel Ángel Rodríguez Dalí <migueadali@gmail.com>
2022-12-14 19:31:22 +0100

    a

--- Acceso a datos/Práctica 3/RodriguezDaliMA-P3/src/main/java/controllers/UsuarioController.java
+++ Acceso a datos/Práctica 3/RodriguezDaliMA-P3/src/main/java/controllers/UsuarioController.java
@@ -1,4 +1,5 @@
 package controllers;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -17,142 +18,136 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.query.Query;
 
 import models.Usuario;
+
 public class UsuarioController {
-	
+
 	public EntityManager manager;
-	
-	public String createUsuario(String nombre, String apellido, String direccion){
-		SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Usuario.class).buildSessionFactory();
-		
+
+	public String createUsuario(String nombre, String apellido, String direccion) {
+		SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Usuario.class).buildSessionFactory();
+
 		Session session = sessionFactory.openSession();
-		
+
 		try {
 			Usuario usuario = new Usuario(nombre, apellido, direccion);
-			
+
 			session.beginTransaction();
-			
+
 			session.save(usuario);
-			
+
 			session.getTransaction().commit();
-			
+
 			session.close();
-			
+
 			return "Usuario Creado";
-		}catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return "Error al registrar el usuario";
 	}
+
 	public String deleteUsuario(int id) {
-		SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Usuario.class).buildSessionFactory();
-		
+		SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Usuario.class).buildSessionFactory();
+
 		Session session = sessionFactory.openSession();
-		
+
 		try {
-			
-			
+
 			session.beginTransaction();
-			
+
 			Usuario usuario = session.get(Usuario.class, id);
-			
+
 			session.delete(usuario);
-			
+
 			session.getTransaction().commit();
-			
+
 			session.close();
-			
+
 			return "Usuario Eliminado";
-		}catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return "Error al eliminar el usuario";
 	}
-	
+
 	public String updateUsuario(int id, String nombre) {
 
-		SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Usuario.class).buildSessionFactory();
-		
+		SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Usuario.class).buildSessionFactory();
+
 		Session session = sessionFactory.openSession();
-		
+
 		try {
-			
-			
+
 			session.beginTransaction();
-			
+
 			Usuario usuario = session.get(Usuario.class, id);
-			
+
 			usuario.setNombre(nombre);
-			
+
 			session.update(usuario);
-			
+
 			session.getTransaction().commit();
-			
+
 			session.close();
-			
+
 			return "Usuario Actualizado";
-		}catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return "Error al actualizar el usuario";
 	}
-	
+
 	public String getUsuario(int id) {
-		SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Usuario.class).buildSessionFactory();
-		
+		SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Usuario.class).buildSessionFactory();
+
 		Session session = sessionFactory.openSession();
-		
+
 		try {
-			
-			
+
 			session.beginTransaction();
-			
+
 			Usuario usuario = session.get(Usuario.class, id);
-			
+
 			session.getTransaction().commit();
-			
+
 			session.close();
-			
+
 			return usuario.toString();
-		}catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return "Error al actualizar el usuario";
 	}
-	
+
 	public String[] getUsuarios() {
-		SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Usuario.class).buildSessionFactory();
-		
+		SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Usuario.class).buildSessionFactory();
+
 		Session session = sessionFactory.openSession();
-		
-		 CriteriaBuilder builder = session.getCriteriaBuilder();
-		 CriteriaQuery<Usuario> criteria = builder.createQuery(Usuario.class);
-		 criteria.from(Usuario.class);
-		 List<Usuario> data = session.createQuery(criteria).getResultList();
-		 
-		 String salida = "";
-		 for(Usuario a : data) {
+
+		CriteriaBuilder builder = session.getCriteriaBuilder();
+		CriteriaQuery<Usuario> criteria = builder.createQuery(Usuario.class);
+		criteria.from(Usuario.class);
+		List<Usuario> data = session.createQuery(criteria).getResultList();
+
+		String salida = "";
+		for (Usuario a : data) {
 //			 System.out.println(a.toString());
-			 salida += a.toString();
-			 salida += ":";
-		 }//Fin Para
-		 
-		 String[] salidaBuena;
-		 salidaBuena = salida.split(":");
-		 return salidaBuena;
+			salida += a.toString();
+			salida += ":";
+		} // Fin Para
+
+		String[] salidaBuena;
+		salidaBuena = salida.split(":");
+		return salidaBuena;
 	}
-	
-	
-	
-	
-	
 
-	
 }
-	
-	
-
-
