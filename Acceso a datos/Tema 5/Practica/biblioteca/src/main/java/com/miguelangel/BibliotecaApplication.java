b7fe3fa26833e7d2c921e3f0eed0779521ced9e3
Miguel Ángel Rodríguez Dalí <migueadali@gmail.com>
2023-01-09 13:46:02 +0100

    base app add p5

--- Acceso a datos/Tema 5/Practica/biblioteca/src/main/java/com/miguelangel/BibliotecaApplication.java
+++ Acceso a datos/Tema 5/Practica/biblioteca/src/main/java/com/miguelangel/BibliotecaApplication.java
@@ -1,20 +1,35 @@
 package com.miguelangel;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.miguelangel.model.Categoria;
+import com.miguelangel.repository.CategoriasRepository;
+
 @SpringBootApplication
 public class BibliotecaApplication implements CommandLineRunner{
 
+	@Autowired
+	private CategoriasRepository categoriasRepo;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BibliotecaApplication.class, args);
-	}
+	}//Fin Programa
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-		
-	}
+		buscarTodosJpa();
+	}//Fin Procedimiento
+	
+	private void buscarTodosJpa(){
+		List<Categoria> categorias = categoriasRepo.findAll();
+		for (Categoria c: categorias) {
+			System.out.println(c.getId() + " " + c.getNombre());
+		}//Fin Paras
+	}//Fin Procedimiento
 
 }
