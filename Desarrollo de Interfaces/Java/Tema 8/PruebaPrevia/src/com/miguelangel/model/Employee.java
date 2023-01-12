33b6b29a33bb080e51eddeec45a2b51989954153
Miguel Ángel Rodríguez Dalí <migueadali@gmail.com>
2023-01-12 10:30:59 +0100

    Avanzada bastante la prueba
    https://youtu.be/fZtnoQpPzaw?t=1286

---
+++ Desarrollo de Interfaces/Java/Tema 8/PruebaPrevia/src/com/miguelangel/model/Employee.java
@@ -0,0 +1,55 @@
+package com.miguelangel.model;
+
+public class Employee {
+	//Atributos:
+	private int id; 
+	private String firstName; 
+	private String lastName;
+	private String address;
+	private double salary;
+	
+	//Constructor:
+	public Employee() {}
+
+	//Métodos:
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	
+}
