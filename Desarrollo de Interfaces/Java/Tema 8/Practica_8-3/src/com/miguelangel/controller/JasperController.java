89f656dd3fde653cbe6e651f1b26afd8adcded88
Miguel Ángel Rodríguez Dalí <migueadali@gmail.com>
2023-01-12 10:17:33 +0100

    Cagadas en el proyecto DDI y reseteo pruebas

--- Desarrollo de Interfaces/Java/Tema 8/Practica_8-3/src/com/miguelangel/controller/JasperController.java
+++ Desarrollo de Interfaces/Java/Tema 8/Practica_8-3/src/com/miguelangel/controller/JasperController.java
@@ -2,32 +2,84 @@ package com.miguelangel.controller;
 
 import java.util.List;
 
+import com.miguelangel.model.Humano;
+
 import net.sf.jasperreports.engine.JRDataSource;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JRField;
 
 public class JasperController implements JRDataSource {
 
-	private List<String[]> datosAMostrar;
+//	private final Object[][] listadoHumanos;
+//	private List<String[]> datosAMostrar;
+	private List<Humano> listaHumanos;
 	private int index;
 
 	public JasperController() {
 		// Entorno:
 		// Algoritmo:
 		this.index = -1;
-		this.datosAMostrar = HumanoController.recoleccionListaDatos(HumanoController.creadorInicial());
+//		this.datosAMostrar = HumanoController.recoleccionListaDatos(HumanoController.creadorInicial());
+		this.listaHumanos = HumanoController.creadorInicial();
+		
+//		this.listadoHumanos = new Object[][] {
+//			this.datosAMostrar.get(0), this.datosAMostrar.get(1), this.datosAMostrar.get(2)
+//		};
 	}//Fin Constrcutor
 
 	@Override
 	public Object getFieldValue(JRField arg0) throws JRException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+
+		Object value = null;
+		
+		String fieldName = arg0.getName();
+		
+		switch(fieldName) {
+		case "nombre":
+//			value = this.listadoHumanos[this.index][0];
+			String[] listadoHumanos = new String[]{
+				this.listaHumanos.get(0).getNombre(), this.listaHumanos.get(1).getNombre(),
+				this.listaHumanos.get(2).getNombre()
+			};
+			
+			
+			value = listadoHumanos;
+			break;
+//		case "apellidos":
+//			value = this.listadoHumanos[this.index][1];
+//			break;
+//		case "fecNac":
+//			value = this.listadoHumanos[this.index][2];
+//			break;
+//		case "lugarNac":
+//			value = this.listadoHumanos[this.index][3];
+//			break;
+//		case "dir":
+//			value = this.listadoHumanos[this.index][4];
+//			break;
+//		case "municipio":
+//			value = this.listadoHumanos[this.index][5];
+//			break;
+//		case "prov":
+//			value = this.listadoHumanos[this.index][6];
+//			break;
+//		case "telf":
+//			value = this.listadoHumanos[this.index][7];
+//			break;
+		}//Fin Según Sea
+		
+		return value;
+	}//Fin Función
 
 	@Override
 	public boolean next() throws JRException {
-		// TODO Auto-generated method stub
+//		this.index++;
+//		return (this.index < datosAMostrar.size());
 		return false;
-	}
+	}//Fin Función
+	
+	public static JRDataSource getDataSource() {
+		return new JasperController();
+	}//Fin Función
 
 }
--- Desarrollo de Interfaces/Java/Tema 8/Practica_8-3/src/com/miguelangel/view/Main.java
+++ Desarrollo de Interfaces/Java/Tema 8/Practica_8-3/src/com/miguelangel/view/Main.java
@@ -1,21 +1,47 @@
 package com.miguelangel.view;
 
-import java.util.List;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
-import com.miguelangel.controller.HumanoController;
+import javax.swing.WindowConstants;
+
+import com.miguelangel.controller.JasperController;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.util.JRLoader;
+import net.sf.jasperreports.view.JasperViewer;
 
 public class Main {
 
+//	public Main() {
+//		
+//		Path filePath = Paths.get("./src/com/miguelangel/reports/Leaf_Violet.jasper");
+//
+//
+//		try {
+//			JasperReport report = (JasperReport) JRLoader
+//					.loadObject(getClass().getResource(String.valueOf("./src/com/miguelangel/reports/Leaf_Violet.jasper")));
+//			JasperPrint jPrint = JasperFillManager.fillReport(report, null, JasperController.getDataSource());
+//
+//			JasperViewer view = new JasperViewer(jPrint, false);
+//			view.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+//			view.setVisible(true);
+//		} catch (JRException ex) {
+//			ex.getMessage();
+//		}//Fin try
+//		
+//	}
+
 	public static void main(String[] args) {
-		//Entorno:
-		HumanoController controlador;
-		List<String[]> datosAMostrar;
-		
-		//Algoritmo:
-		controlador = new HumanoController();
-		datosAMostrar = controlador.recoleccionListaDatos(controlador.creadorInicial());
-		
-		
+		// Entorno:
+
+		// Algoritmo:
+		Path filePath = Paths.get("./src/com/miguelangel/reports/Leaf_Violet.jasper");
+		System.err.println(filePath);
+//		Main jasper = new Main();
 	}
 
 }
